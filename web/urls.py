cec8dd737dc2198b94e940a1350b0c3a760db398
Oleg <pakolegdev@gmail.com>
2022-12-14 00:13:53 +0300

    frontend changes

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, SearchResultView, CarDetailView
+from .views import SearchResultView, CarDetailView, FilterResultView , HomePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("result", SearchResultView.as_view(), name="search_results"),
+    path("filter", FilterResultView.as_view(), name="filter_results"),
     path("car/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
 ]
--- web/views.py
+++ web/views.py
@@ -3,21 +3,54 @@ from .models import Car
 from django.db.models import Q
 
 
-class HomePageView(TemplateView):
+# View for starting page
+class HomePageView(ListView):
+    model = Car
     template_name = 'index.html'
 
+    # Override get_context_data for passing CAR and CAR.CATEGORIES models
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['categories'] = [e.value for e in Car.BodyStyle]
+        return context
 
+
+# View for retrieving result of the SEARCH
 class SearchResultView(ListView):
     model = Car
     template_name = 'result-page.html'
 
+    # function to search car by name
     def get_queryset(self):
+        # get 'query' from the input in the navbar search section
         query = self.request.GET.get('query')
         return Car.objects.filter(
-            Q(description__icontains=query) or Q(make__icontains=query) or Q(name__icontains=query)
+            Q(name__icontains=query)
         )
 
 
+# View for retrieving result of FILTERING
+class FilterResultView(ListView):
+    model = Car
+    template_name = 'result-page.html'
+
+    # Use possibilities of QuerySet to implement filtering
+    def get_queryset(self):
+        state = self.request.GET.get('state')
+        make = self.request.GET.get('make')
+        price = self.request.GET.get('price')
+        mileage = self.request.GET.get('mileage')
+        return Car.objects.filter(
+            Q(state__iexact=state) and Q(make__iexact=make) and Q(price__lte=price) or Q(mileage__lte=mileage)
+        )
+
+
+# View for representing the details of the CAR
 class CarDetailView(DetailView):
     model = Car
     template_name = "details-page.html"
+    # Override get_context_data for passing individual CAR and list of CAR models
+    def get_context_data(self, **kwargs):
+        context = super(CarDetailView, self).get_context_data(**kwargs)
+        context['car_list'] = Car.objects.all()
+        return context
