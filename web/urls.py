1cf8bf1f2d159a7ff41e56f8bd67d1f9e099ae0b
Oleg <pakolegdev@gmail.com>
2022-12-21 13:23:30 +0300

    add links to other pages in templates

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import SearchResultView, CarDetailView, FilterResultView, HomePageView, FullSearchView, AdvancedSearchView, \
-    CategoryFilterView, SignUpView, UserProfile
+    CategoryFilterView, SignUpView, UserProfile, Favourites
+from . import views
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
@@ -11,5 +12,7 @@ urlpatterns = [
     path("category", CategoryFilterView.as_view(), name="category_filter"),
     path("full_filter", FullSearchView.as_view(), name="full_filter_results"),
     path("car/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
+    path("fav/<int:id>/", views.favourite_add, name="favourite_add"),
+    path("favourites", Favourites.as_view(), name="favourites"),
     path("advanced_search", AdvancedSearchView.as_view(), name="advanced_search"),
 ]
